8924a40532c8e0d8a073651f2f3bbdc694fd314a
Jessica Teekasingh <jessi@jessicas-mbp.attlocal.net>
2020-05-16 11:12:26 -0400

    updating file location

---
+++ r_and_m_app_proj/js/app.js
@@ -0,0 +1,25 @@
+//create 3 buttons for Rick, Morty, & Jerry
+// buttons will get data from API & display it in console
+//then begin to filter
+
+
+
+console.log($);
+
+//
+// $(() => {
+//   $('.button').on('click', () => {
+//     event.preventDefault()
+//
+//     let link = `https://rickandmortyapi.com/api/character`
+//
+//     $.ajax(
+//       {
+//       url: link,
+//       type: 'GET'
+//       }
+//     ).then((info) => {
+//       console.log(info);
+//     })
+//   })
+// })//end of DOM
